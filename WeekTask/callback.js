84ad354983fbb8751f3bc68b70f822bca9e4c797
pachputeshrutika21 <52059643+pachputeshrutika21@users.noreply.github.com>
2020-03-02 10:44:10 +0530

    Add files via upload

---
+++ WeekTask/callback.js
@@ -0,0 +1,24 @@
+const user2=[];
+
+function userInfo()
+{
+    usersDetails(users);
+}
+async function usersDetails(callback)
+{ 
+const response = await fetch("https://api.github.com/users");
+const data= await response.json();
+callback(data); 
+} 
+function userInformation(data)
+{
+ try
+ {
+  data.info.map((data1)=>user2.push(data1));
+  console.log("callback!!!!");
+  console.log(JSON.stringify(user2));
+}
+catch{
+    console.log("Oh No!!!");
+}
+}
